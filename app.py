ca5a1c992123af173ea94bb74b69a17a981cff2e
molmatt <mol.matthew.j@gmail.com>
2018-10-03 09:49:47 -0500

    tweaking for heroku

--- app.py
+++ app.py
@@ -152,9 +152,7 @@ NYMdf = pd.read_csv('NYMuseums.csv')
 #Reading in the trained convolutional neural network VGG16 transfer trained on
 #art pictures and not art, also setting up stuff for filtering
 
-vgg_conv = VGG16(weights='imagenet',
-                  include_top=False,
-                  input_shape=(224, 224, 3))
+vgg_conv = keras.models.load_model('firsthalf.h5')
 vgg_conv._make_predict_function()
 ArtClassy = keras.models.load_model('ArtClass.h5')
 ArtClassy._make_predict_function()
